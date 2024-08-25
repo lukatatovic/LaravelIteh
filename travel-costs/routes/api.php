4aa2fabe7ac069cf145776989dc13e988a553a3a
TeodoraTrkulja <teodoratrkulja1@gmail.com>
2024-08-25 18:14:36 +0200

    Created APIControleer and added routes.

--- travel-costs/routes/api.php
+++ travel-costs/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Authorization\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [APIController::class, 'register']);
+Route::post('/login', [APIController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    Route::post('/logout', [APIController::class, 'logout']);
 });
